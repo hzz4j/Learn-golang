448f95cb2b58ba74faad77fed10573f6276608e0
静默 <cau1403090523@gmail.com>
2022-10-10 19:25:23 +0800

    Create if.go

---
+++ basic/06 if/if.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
+
+/**
+9 has 1 digit
+*/
