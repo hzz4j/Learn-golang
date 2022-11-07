caacc3837691fc93f5034c192b5a52dfc553c3d9
静默 <cau1403090523@gmail.com>
2022-11-08 02:07:08 +0800

    finished go array

---
+++ basic/07 array/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	// declareArray1()
+	// declareArray2()
+	iterateArray()
+}
+
+func declareArray1() {
+	var a1 [5]int
+	var a2 [3]string
+	fmt.Println(a1) // [0 0 0 0 0]
+	fmt.Println(a2) // [  ]
+}
+
+func declareArray2() {
+	a1 := [3]int{1, 2, 3}
+	// 如果将元素个数指定为特殊符号...，则表示通过初始化时的给定的值个数来推断数组长度
+	a2 := [...]int{1, 2, 3, 4, 5}
+	// 如果声明数组时，只想给其中某几个元素初始化赋值，则使用索引号
+	a3 := [...]int{0: 1, 7: 7}
+	fmt.Println(a1) // [1 2 3]
+	fmt.Println(a2) // [1 2 3 4 5]
+	fmt.Println(a3) // [1 0 0 0 0 0 0 7]
+}
+
+// 数组类型
+func arrayType() {
+	var (
+		a [3]int
+		b [5]int
+	)
+
+	fmt.Println(reflect.TypeOf(a)) // [3]int
+	fmt.Println(reflect.TypeOf(b)) // [5]int
+}
+
+// 遍历数组
+func iterateArray() {
+	a := [...]int{3, 2, 5, 6}
+	for i, v := range a {
+		fmt.Println(i, v)
+	}
+}
